97d5d88e1049b923e851eb2291f257cc288a998f
ZakirAlekperov <zakiralekperov1998@gmail.com>
2026-02-13 14:49:03 +0300

    fix: correct UploadPlanImageUseCase import path

--- src/main/java/zakir/alekperov/ui/tabs/locationplan/LocationPlanTabController.java
+++ src/main/java/zakir/alekperov/ui/tabs/locationplan/LocationPlanTabController.java
@@ -17,6 +17,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import zakir.alekperov.application.locationplan.*;
+import zakir.alekperov.application.locationplan.usecases.UploadPlanImageUseCase;  // 🔧 Явный import!
 import zakir.alekperov.ui.dialogs.AddBuildingDialogController;
 import zakir.alekperov.ui.dialogs.ExportDialog;
 import zakir.alekperov.ui.export.CanvasExporter;
